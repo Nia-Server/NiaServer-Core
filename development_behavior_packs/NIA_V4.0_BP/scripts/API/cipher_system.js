40933b85ed7aad1d0088b80d1b11a0d0db39b01e
jiansyuan <jiansyuan@qq.com>
2023-06-28 17:03:28 +0800

    fix BigInt problem

--- development_behavior_packs/NIA_V4.0_BP/scripts/API/cipher_system.js
+++ development_behavior_packs/NIA_V4.0_BP/scripts/API/cipher_system.js
@@ -3,7 +3,7 @@ function __adler32(str) {
     var P = 65521, a = 1, b = 0;  
     str = (new TextEncoder('utf8')).encode(str);
     for(var i=0;i < str.length; i++) a = (a+str[i])%P, b = (a+b)%P; 
-    return ((BigInt(b) * BigInt(1<<16)) + BigInt(a));  
+    return (b>>>0)*((1<<16)>>>0)+(a>>>0);
 }
 function adler32(str){
     str = __adler32(str);
