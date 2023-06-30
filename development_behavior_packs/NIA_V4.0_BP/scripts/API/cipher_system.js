8abd231816a60c0ac42f7b8e4e5a16f2b3e5ca9e
jiansyuan <jiansyuan@qq.com>
2023-06-30 18:26:47 +0800

    fix

--- development_behavior_packs/NIA_V4.0_BP/scripts/API/cipher_system.js
+++ development_behavior_packs/NIA_V4.0_BP/scripts/API/cipher_system.js
@@ -1,9 +1,9 @@
 
 function __adler32(str) {  
     var P = 65521, a = 1, b = 0;  
-    str = (new TextEncoder('utf8')).encode(str);
-    for(var i=0;i < str.length; i++) a = (a+str[i])%P, b = (a+b)%P; 
-    return (b>>>0)*((1<<16)>>>0)+(a>>>0);
+    str = eval('\''+encodeURI(str).replace(/%/gm, '\\x')+'\'');
+    for(var i=0;i < str.length; i++) a = (a+str.charCodeAt(i))%P, b = (a+b)%P; 
+    return (b>>>0)*((1<<16)>>>0)+(a>>>0); 
 }
 function adler32(str){
     str = __adler32(str);
