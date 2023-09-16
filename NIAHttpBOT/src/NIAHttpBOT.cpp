f8ecd8de008a01b67be1d68fb0056e86257e3809
jiansyuan <jiansyuan@qq.com>
2023-09-16 23:11:38 +0800

    multithread sync

--- NIAHttpBOT/src/NIAHttpBOT.cpp
+++ NIAHttpBOT/src/NIAHttpBOT.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <filesystem>
+#include <syncstream>
 
 #include <httplib.h>
 #include <rapidjson/document.h>
@@ -15,20 +16,22 @@
 #include "CFG_Parser.hpp"
 #include "I18Nize.hpp"
 
-#define INFO(a) GetTime(), std::cout<<"\x1b[32m[INFO]\x1b[0m "<<a<<std::endl
-#define WARN(a) GetTime(), std::cout<<"\x1b[43;1m[WARN]\x1b[0m "<<a<<std::endl
+#define SYNC(a) std::basic_osyncstream(a)
+
+#define INFO(a) GetTime(), SYNC(std::cout)<<"\x1b[32m[INFO]\x1b[0m "<<a<<std::endl
+#define WARN(a) GetTime(), SYNC(std::cout)<<"\x1b[43;1m[WARN]\x1b[0m "<<a<<std::endl
 #define REMOVE_PATH(a) (a + sizeof(__PROJECT__) -1)
-#define FAIL(a) GetTime(), std::cout<<"\x1b[41;1m[FAIL]\x1b[0m \x1b[36;45;4mError at " \
+#define FAIL(a) GetTime(), SYNC(std::cout)<<"\x1b[41;1m[FAIL]\x1b[0m \x1b[36;45;4mError at " \
 	<<REMOVE_PATH(__FILE__)<<":"<<__LINE__<<" ("<<__FUNCTION__ \
 	<<")\x1b[0m ==>\n"<<a<<'\n'<<std::endl
-#define LOG(sym,str) GetTime(), std::cout<<#sym" "<<str<<std::endl
+#define LOG(sym,str) GetTime(), SYNC(std::cout)<<#sym" "<<str<<std::endl
 
-#define XINFO(a) GetTime(), std::cout<<"\x1b[32m[INFO]\x1b[0m "<<__I18N(a)<<std::endl
-#define XWARN(a) GetTime(), std::cout<<"\x1b[43;1m[WARN]\x1b[0m "<<__I18N(a)<<std::endl
-#define XFAIL(a) GetTime(), std::cout<<"\x1b[41;1m[FAIL]\x1b[0m \x1b[36;45;4mError at " \
+#define XINFO(a) GetTime(), SYNC(std::cout)<<"\x1b[32m[INFO]\x1b[0m "<<__I18N(a)<<std::endl
+#define XWARN(a) GetTime(), SYNC(std::cout)<<"\x1b[43;1m[WARN]\x1b[0m "<<__I18N(a)<<std::endl
+#define XFAIL(a) GetTime(), SYNC(std::cout)<<"\x1b[41;1m[FAIL]\x1b[0m \x1b[36;45;4mError at " \
 	<<REMOVE_PATH(__FILE__)<<":"<<__LINE__<<" ("<<__FUNCTION__ \
 	<<")\x1b[0m ==>\n"<<__I18N(a)<<'\n'<<std::endl
-#define XLOG(sym,str) GetTime(), std::cout<<#sym" "<<__I18N(str)<<std::endl
+#define XLOG(sym,str) GetTime(), SYNC(std::cout)<<#sym" "<<__I18N(str)<<std::endl
 #define X(a) __I18N(a)
 
 //初始化配置文件数据
@@ -37,7 +40,7 @@ int PORT = 10086;
 bool UseCmd = false;
 
 
-void GetTime() {
+inline void GetTime() {
 	time_t timep; tm p;
     char time_buffer[80];
 	time(&timep);
@@ -47,7 +50,7 @@ void GetTime() {
 	localtime_r(&timep, &p);
 #endif
     strftime(time_buffer, sizeof(time_buffer), "[%Y/%m/%d %H:%M:%S] ", &p);
-	std::cout << "\x1b[35m" << time_buffer << "\x1b[0m";
+	SYNC(std::cout) << "\x1b[35m" << time_buffer << "\x1b[0m";
 }
 
 int main() {
